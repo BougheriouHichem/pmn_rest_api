3c6e424f4f421b6360e5c3977f65487f28a42106
bougheriouHichem <bougheriou1997@gmail.com>
2023-12-11 15:30:30 +0100

    first commit

---
+++ src/main/java/com/example/pmnapirest/PmnapirestApplication.java
@@ -0,0 +1,13 @@
+package com.example.pmnapirest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PmnapirestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PmnapirestApplication.class, args);
+	}
+
+}
